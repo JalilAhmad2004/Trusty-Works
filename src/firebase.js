7536d43b3864b135fd3a3e10d360a7466bbe1425
M. Jalil Ahmad <117964245+JalilAhmad2004@users.noreply.github.com>
2025-03-09 01:10:30 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore'; // If you want Firestore late
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVnI2OZmAzi_XZt3M_Hjm9UnJJRx_oZFc",
-  authDomain: "trusty-works.firebaseapp.com",
-  projectId: "trusty-works",
-  storageBucket: "trusty-works.firebasestorage.app",
-  messagingSenderId: "993436578793",
-  appId: "1:993436578793:web:5f0337816636aed0f6d178",
-  measurementId: "G-T9XEE5DJGS"
+  apiKey: "your api key",
+  authDomain: "your doamin",
+  projectId: "your project id",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
